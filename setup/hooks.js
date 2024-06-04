b15f1b2b94a5efbbbae931962781a895888f9316
Benedetta-fabbri <benedetta11.fabbri@emea.nttdata.com>
2024-06-04 10:36:06 +0200

    P4ADEV-357 added config

---
+++ setup/hooks.js
@@ -0,0 +1,40 @@
+const { BeforeAll, Before, AfterAll, After, Status } = require ('cucumber')
+const { chromium } = require('playwright');
+
+const options = {
+  headless: false,
+  slowMo: 100
+};
+
+// Create a global browser for the test session.
+BeforeAll(async () => {
+  console.log('Before all ...');
+  global.browser = await chromium.launch(options);
+});
+
+AfterAll(async () => {
+  console.log('After all ...');
+  await global.browser.close();
+});
+
+// Create a fresh browser context for each test.
+Before(async () => {
+  console.log('Before ...');
+  global.context = await global.browser.newContext();
+  global.page = await global.context.newPage();
+});
+
+// close the page and context after each test.
+After(async () => {
+  console.log('After pass...');
+  await global.page.close();
+  await global.context.close();
+});
+
+
+After(async function (scenario) {
+  if (scenario.result.status === Status.FAILED) {
+    var buffer = await global.page.screenshot({ path: `report/${scenario.pickle.name}.png`, fullPage: true })
+    this.attach(buffer, 'image/png');
+  }
+});

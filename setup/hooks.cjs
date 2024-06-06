2469aa85af0282585115c2f29cb9d9c8bb8c1ace
Benedetta-fabbri <benedetta11.fabbri@emea.nttdata.com>
2024-06-06 09:49:05 +0200

    P4ADEV-347 add cucumber formatter

--- setup/hooks.cjs
+++ setup/hooks.cjs
@@ -10,25 +10,21 @@ global.baseUrl = 'https://payhub.dev.p4pa.pagopa.it/myoperatore/home';
 
 // Create a global browser for the test session.
 BeforeAll(async () => {
-  console.log('Before all ...');
   global.browser = await chromium.launch(options);
 });
 
 AfterAll(async () => {
-  console.log('After all ...');
   await global.browser.close();
 });
 
 // Create a fresh browser context for each test.
 Before(async () => {
-  console.log('Before ...');
   global.context = await global.browser.newContext();
   global.page = await global.context.newPage();
 });
 
 // close the page and context after each test.
 After(async () => {
-  console.log('After pass...');
   await global.page.close();
   await global.context.close();
 });
